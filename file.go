3b816fbb7360a455607f0a684e81950fc6d20edb
loveyu <loveyu22333@gmail.com>
2026-01-07 14:08:39 +0800

    feat(resty): 添加文件上传和HTTP请求快捷方法
    - 新增 UploadFileToTargetURL 函数支持文件上传到指定URL
    - 添加多种上传文件选项配置函数如 WithUploadFileKey、WithUploadFileValue 等
    - 实现 RPost 和 RGet 快捷请求方法
    - 修改默认 Resty 客户端超时时间为5秒，移除重试机制
    - 增加请求头、查询参数、认证令牌等便捷设置功能

--- file.go
+++ file.go
@@ -3,6 +3,8 @@ package wd
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -65,4 +67,153 @@ func GetCurrentLine() (folderName string, fileName string, funcName string, line
 	folderName = filepath.Base(filepath.Dir(file))
 
 	return
+}
+
+type UploadFileReq struct {
+	fileKey           string
+	fileValue         *multipart.FileHeader
+	OtherParams       map[string]string
+	Headers           map[string]string
+	token             string
+	url               string
+	resp              any
+	fileNameGen       func(fileName string) string
+	FileName          string
+	beforeRequestFunc func(req *UploadFileReq)
+}
+
+type UploadFileOption func(*UploadFileReq)
+
+// WithUploadFileKey 自定义上传文件的表单键名。
+func WithUploadFileKey(key string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.fileKey = key
+	}
+}
+
+// WithUploadBeforeRequestFunc 请求调用前，参数组装后的钩子函数
+func WithUploadBeforeRequestFunc(after func(req *UploadFileReq)) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.beforeRequestFunc = after
+	}
+}
+
+// WithUploadFileValue 指定需要上传的文件内容。
+func WithUploadFileValue(file *multipart.FileHeader) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.fileValue = file
+	}
+}
+
+// WithUploadFileFormData 设置额外的表单参数，后设置的值会覆盖同名键。
+func WithUploadFileFormData(params map[string]string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		if req.OtherParams == nil {
+			req.OtherParams = map[string]string{}
+		}
+		for k, v := range params {
+			req.OtherParams[k] = v
+		}
+	}
+}
+
+// WithUploadFileHeaders 添加自定义请求头。
+func WithUploadFileHeaders(headers map[string]string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		if req.Headers == nil {
+			req.Headers = map[string]string{}
+		}
+		for k, v := range headers {
+			req.Headers[k] = v
+		}
+	}
+}
+
+// WithUploadFileToken 添加token信息。
+func WithUploadFileToken(token string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.token = token
+	}
+}
+
+// WithUploadFileURL 指定目标地址。
+func WithUploadFileURL(url string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.url = url
+	}
+}
+
+// WithUploadFileResp 指定响应结果需要反序列化到的对象。
+func WithUploadFileResp(resp any) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.resp = resp
+	}
+}
+
+// WithUploadFileNameGenerator 提供自定义的文件名生成器。
+func WithUploadFileNameGenerator(gen func(fileName string) string) UploadFileOption {
+	return func(req *UploadFileReq) {
+		req.fileNameGen = gen
+	}
+}
+
+// UploadFileToTargetURL 上传文件到指定url的快捷操作
+func UploadFileToTargetURL(options ...UploadFileOption) error {
+	req := UploadFileReq{
+		fileKey:     "file",
+		OtherParams: map[string]string{},
+	}
+	for _, option := range options {
+		if option != nil {
+			option(&req)
+		}
+	}
+
+	if req.resp == nil || !IsPtr(req.resp) {
+		return errors.New("value必须为指针")
+	}
+	if req.fileValue == nil {
+		return errors.New("file为空")
+	}
+	if req.OtherParams == nil {
+		req.OtherParams = map[string]string{}
+	}
+	if req.fileNameGen == nil {
+		req.fileNameGen = func(fileName string) string {
+			fileType := GetFileNameType(req.fileValue.Filename)
+			return fmt.Sprintf("%s.%s", GetUUID(), fileType)
+		}
+	}
+	req.FileName = req.fileNameGen(req.fileValue.Filename)
+
+	if req.beforeRequestFunc != nil {
+		req.beforeRequestFunc(&req)
+	}
+
+	open, err := req.fileValue.Open()
+	if err != nil {
+		return err
+	}
+	request := R().
+		SetFileReader(req.fileKey, req.FileName, open).
+		SetFormData(req.OtherParams)
+
+	if req.token != "" {
+		request = request.SetAuthToken(strings.TrimSpace(strings.TrimPrefix(req.token, "Bearer")))
+	}
+	if len(req.Headers) > 0 {
+		request = request.SetHeaders(req.Headers)
+	}
+
+	resp, err := request.
+		Post(req.url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		return errors.New("上传失败，响应状态码为：" + resp.Status())
+	}
+
+	return json.Unmarshal(resp.Body(), req.resp)
 }
--- resty.go
+++ resty.go
@@ -1,6 +1,10 @@
 package wd
 
 import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
 	"sync"
 	"time"
 
@@ -16,10 +20,8 @@ var (
 func initRestyClient() {
 	defaultRestyClient = resty.New()
 	defaultRestyClient.
-		SetTimeout(30 * time.Second).
-		SetRetryCount(2).
-		SetRetryWaitTime(500 * time.Millisecond).
-		SetRetryMaxWaitTime(2 * time.Second)
+		SetTimeout(5 * time.Second).
+		SetRetryCount(0)
 }
 
 // RestyClient 用来返回带懒加载的 Resty 单例。
@@ -31,4 +33,44 @@ func RestyClient() *resty.Client {
 // R 用来基于默认客户端创建请求。
 func R() *resty.Request {
 	return RestyClient().R()
+}
+
+// RPost 快捷的post请求，参数headers为请求头，body为请求体，url为请求地址，value为返回值必须是指针，token按需传递
+func RPost(headers map[string]string, body interface{}, url string, value any, token ...string) error {
+	if !IsPtr(value) {
+		return errors.New("value必须是一个指针")
+	}
+	request := R().SetHeaders(headers).SetBody(body)
+	if len(token) > 0 {
+		request = request.SetAuthToken(strings.TrimSpace(strings.TrimPrefix(token[0], "Bearer")))
+	}
+	resp, err := request.Post(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return errors.New("请求失败，状态码为：" + resp.Status())
+	}
+
+	return json.Unmarshal(resp.Body(), value)
+}
+
+// RGet 快捷的get请求，参数headers为请求头，query为请求体，url为请求地址，value为返回值必须是指针，token按需传递
+func RGet(headers map[string]string, query map[string]string, url string, value any, token ...string) error {
+	if !IsPtr(value) {
+		return errors.New("value必须是一个指针")
+	}
+	request := R().SetHeaders(headers).SetQueryParams(query)
+	if len(token) > 0 {
+		request = request.SetAuthToken(strings.TrimSpace(strings.TrimPrefix(token[0], "Bearer")))
+	}
+	resp, err := request.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode() != http.StatusOK {
+		return errors.New("请求失败，状态码为：" + resp.Status())
+	}
+
+	return json.Unmarshal(resp.Body(), value)
 }
