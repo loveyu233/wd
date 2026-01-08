fd359edb36a1b0112ed78a444cd549b2dd4caa3c
loveyu <loveyu22333@gmail.com>
2026-01-08 16:38:53 +0800

    refactor(middleware): 修改恢复中间件的日志处理机制
    - 移除自定义日志接口和默认日志实现
    - 将日志参数改为函数类型回调机制
    - 重构panic捕获逻辑以支持函数式日志处理
    - 保持原有的错误响应和状态码返回功能

--- middleware_recovery.go
+++ middleware_recovery.go
@@ -8,18 +8,17 @@ import (
 )
 
 // MiddlewareRecovery 用来捕获请求中的 panic 并返回统一错误响应。
-func MiddlewareRecovery(log ...CustomLog) gin.HandlerFunc {
-	var loclLog CustomLog
-
-	if len(log) > 0 {
-		loclLog = log[0]
-	} else {
-		loclLog = new(CustomDefaultLogger)
+func MiddlewareRecovery(log ...func(recoverErr any, debugStack []byte)) gin.HandlerFunc {
+	var isFunc bool
+	if len(log) > 0 && log[0] != nil {
+		isFunc = true
 	}
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
-				loclLog.Errorf("panic:%s;stack:%s", err, string(debug.Stack()))
+				if isFunc {
+					log[0](err, debug.Stack())
+				}
 				ResponseError(c, ErrServerBusy.WithMessage("panic:%v", err))
 				c.AbortWithStatus(http.StatusOK)
 			}
